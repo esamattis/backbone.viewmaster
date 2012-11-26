64b04dbf291b81c4e7c7eae8f528115d36e42bfd
Esa-Matti Suuronen <esa-matti@suuronen.org>
2012-11-26 22:17:43 +0200

    Move files to lib

--- grunt.js
+++ grunt.js
@@ -3,12 +3,19 @@
 module.exports = function(grunt) {
 
   grunt.initConfig({
+
     min: {
       dist: {
         src: ["backbone.viewmaster.js"],
         dest: "backbone.viewmaster.min.js"
       }
+    },
+
+    watch: {
+      files: "lib/*",
+      tasks: "default"
     }
+
   });
 
   grunt.registerTask("default", "min");
