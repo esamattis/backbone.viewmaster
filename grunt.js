820ed332ebc1f30e32357add3be592ad63e9e0c9
Esa-Matti Suuronen <esa-matti@suuronen.org>
2012-12-03 21:39:06 +0200

    Build public docs with grunt

--- grunt.js
+++ grunt.js
@@ -1,26 +1,48 @@
 /*jshint node:true*/
 
+var fs = require("fs");
+var version = fs.readFileSync("./lib/backbone.viewmaster.js")
+  .toString().match(/VERSION.*\"([0-9\.]+)\"/)[1];
+
 module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-yuidoc');
   grunt.loadNpmTasks('grunt-exec');
+  grunt.loadNpmTasks('grunt-templater');
+  grunt.loadNpmTasks('grunt-contrib-copy');
   var pkg = require("./package.json");
 
   grunt.initConfig({
-    exec: {
-      pushdoc: {
-        command: "git push -f origin master:gh-pages"
-      }
-    },
+
     yuidoc: {
       compile: {
         name: pkg.name,
         description: pkg.description,
-        version: pkg.version,
+        version: version,
         url: pkg.url,
-        themedir: ".yuidoc_theme",
+        themedir: "public_source/yuidoc-theme",
         options: {
           paths: "lib",
-          outdir: "docs"
+          outdir: "public"
+        }
+      }
+    },
+
+    template: {
+      doc: {
+        src: 'public_source/template.html',
+        dest: 'public/index.html',
+        engine: "underscore",
+        variables: {
+          version: version,
+          body: fs.readFileSync("./README.md").toString()
+        }
+      }
+    },
+
+    copy: {
+      dist: {
+        files: {
+          "./public/": ["./lib/*.js", "./public_source/vendor/**"]
         }
       }
     },
@@ -39,5 +61,5 @@ module.exports = function(grunt) {
 
   });
 
-  grunt.registerTask("default", "min yuidoc");
+  grunt.registerTask("default", "min yuidoc template copy");
 };
